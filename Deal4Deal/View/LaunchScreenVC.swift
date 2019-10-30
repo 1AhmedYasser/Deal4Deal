d5730dd9c6c67f10b878c1ca4047e9549f0c2325
1AhmedYasser <ahmedyasser.dev@hotmail.com>
2019-10-31 00:25:19 +0200

    changes

---
+++ Deal4Deal/View/LaunchScreenVC.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchScreenVC.swift
+//  Deal4Deal
+//
+//  Created by Ahmed yasser on 10/31/19.
+//  Copyright © 2019 IMac. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreenVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
